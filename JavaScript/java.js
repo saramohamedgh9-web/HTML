316fee0e68c51659b4bcc2a1802af135f56560ee
saramohamedgh9-web <saramohamedgh9@gmail.com>
2025-12-20 12:24:42 +0300

    Methods and Lopps

--- JavaScript/java.js
+++ JavaScript/java.js
@@ -1,1 +1,8 @@
-console.log(Math.floor(4.99))
+let x = (Math.random()) * 10;
+let z = Math.ceil(x)
+console.log(z);
+let y = (Math.random()) * 10;
+let w = Math.floor(y)
+console.log(w);
+let a = (Math.random()) * 10;
+let b = Math.round(a)
