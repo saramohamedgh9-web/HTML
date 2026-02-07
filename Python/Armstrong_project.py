ec5784912c2fce0a0572a4f16ae7996baafccc87
saramohamedgh9-web <saramohamedgh9@gmail.com>
2026-02-07 14:07:57 +0300

    Armstrong project

---
+++ Python/Armstrong_project.py
@@ -0,0 +1,8 @@
+# Python program to check if the number is a positive number or not
+# Take input from the user
+num = int(input("Enter a number: "))
+# Check if the number is greater than 0
+if num > 0:
+    print("The number is positive.")
+else:
+    print("The number is not positive.")
