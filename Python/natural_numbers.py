fb5d6b71b321cbcdb314248bda325f38a3650e7e
saramohamedgh9-web <saramohamedgh9@gmail.com>
2026-02-07 15:44:20 +0300

    Activites for the loops class

---
+++ Python/natural_numbers.py
@@ -0,0 +1,4 @@
+# Python program to calculate the sum of the first ten natural numbers
+# Use built-in sum and range functions to calculate the sum
+total_sum = sum(range(1, 11))
+print(f"The sum of the first ten natural numbers is {total_sum}")
