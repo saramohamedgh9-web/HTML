2a796817ae6b321d5906dc2083601ea759eb1c55
saramohamedgh9-web <saramohamedgh9@gmail.com>
2026-03-08 23:47:52 +0300

    file handling project

--- file_handling_project.py
+++ file_handling_project.py
@@ -1,10 +1,30 @@
-# open file and store file object in a variable
-file = open('file_handling_project.txt')
-
-# read the contents of file
-print(file.read())
-
-# close the file
-file.close()
+firstfile = input("Enter the name of first file ")
+secondfile = input("Enter the name of second file ")
 
 
+f1 = open(firstfile, 'r')
+f2 = open(secondfile, 'r')
+
+
+print('content of first file before appending -\n', f1.read())
+print('content of second file before appending - \n', f2.read())
+
+f1.close()
+f2.close()
+
+
+f1 = open(firstfile, 'a+')
+f2 = open(secondfile, 'r')
+
+
+f1.write(f2.read())
+
+
+f1.seek(0)
+f2.seek(0)
+
+print('content of first file after appending - \n', f1.read())
+print('content of second file after appending - \n', f2.read())
+
+f1.close()
+f2.close()
